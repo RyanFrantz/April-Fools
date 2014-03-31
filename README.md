c3851be46a2371969687241b33fdaa598b0c6cab
Ryan Frantz <rfrantz@etsy.com>
2014-03-31 19:46:11 +0000

    Updating the echo statements to properly backspace for the spinner.
    - Updated the README with a link to this script in action.

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-April-Fools
-===========
+# April-Fools
 
 April Fools scripts.  For fun.
+
+## In Action
+Watch [aprilFools.sh](https://asciinema.org/a/8597) this script in action.
--- aprilFools.sh
+++ aprilFools.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-#
-# aprilFools - pretend to be a haX0r script and freak out the TS folk... -ryanfrantz 3/30/2010
-#  call this from a user's .bash_profile and watch them squirm
-#  spelling and grammer are purposely mangled for effect; there's also a clue in the output that this isn't a hack
-#
- 
 control_c() {
   # run if user hits control-C
   echo -en "\n\n!!!!!!!!!!!!!!!!!!!!"
@@ -21,36 +15,36 @@ control_z() {
 } # end control_z()
  
 spinner(){
-	maxCount=$1
-	sleepTime=$2
-	count=1
-	while [ "$count" -le "$maxCount" ];do
-		#echo -n ".." ; sleep 0.1
-		echo -n ".." ; sleep $sleepTime
-		echo -n "/" ; sleep $sleepTime
-		echo -n "-" ; sleep $sleepTime
-		echo -n "\\" ; sleep $sleepTime
-		echo -n "|" ; sleep $sleepTime
-		echo -n "/" ; sleep $sleepTime
-		echo -n "-" ; sleep $sleepTime
-		echo -n "\\" ; sleep $sleepTime
-		echo -n "|" ; sleep $sleepTime
-		echo -n "."
-		count=`expr $count + 1`
-	done
-	return 0
-}	# end spinner()
+    maxCount=$1
+    sleepTime=$2
+    count=1
+    while [ "$count" -le "$maxCount" ];do
+        #echo -n ".." ; sleep 0.1
+        echo -ne "\b.." ; sleep $sleepTime
+        echo -ne "\b/" ; sleep $sleepTime
+        echo -ne "\b-" ; sleep $sleepTime
+        echo -ne "\b\\" ; sleep $sleepTime
+        echo -ne "\b|" ; sleep $sleepTime
+        echo -ne "\b/" ; sleep $sleepTime
+        echo -ne "\b-" ; sleep $sleepTime
+        echo -ne "\b\\" ; sleep $sleepTime
+        echo -ne "\b|" ; sleep $sleepTime
+        echo -ne "\b."
+        count=`expr $count + 1`
+    done
+    return 0
+}   # end spinner()
 
 endlessSpinner(){
-	sleepTime=$1
-	while [ true ];do
-		echo -n "/" ; sleep $sleepTime
-		echo -n "-" ; sleep $sleepTime
-		echo -n "\\" ; sleep $sleepTime
-		echo -n "|" ; sleep $sleepTime
-	done
-	return 0
-}	# end spinner()
+    sleepTime=$1
+    while [ true ];do
+        echo -ne "\b/" ; sleep $sleepTime
+        echo -ne "\b-" ; sleep $sleepTime
+        echo -ne "\b\\" ; sleep $sleepTime
+        echo -ne "\b|" ; sleep $sleepTime
+    done
+    return 0
+}   # end spinner()
 
 
 chr()
@@ -60,56 +54,56 @@ chr()
 }
 
 genFakeKey() {
-	maxCount=$1
-	count=1
-	while [ "$count" -le "$maxCount" ];do
-		# use the built-in $RANDOM variable to generate pseudo-random integer and convert to ASCII
-		chr $RANDOM + $RANDOM + $RANDOM + $RANDOM
-		sleep 0.1
-		count=`expr $count + 1`
-	done
+    maxCount=$1
+    count=1
+    while [ "$count" -le "$maxCount" ];do
+        # use the built-in $RANDOM variable to generate pseudo-random integer and convert to ASCII
+        chr $RANDOM + $RANDOM + $RANDOM + $RANDOM
+        sleep 0.1
+        count=`expr $count + 1`
+    done
 
-}	# end genFakeKey()
+}   # end genFakeKey()
 
 # trap keyboard interrupt (control-c) and job stop (control-z)
 trap control_c SIGINT
 trap control_z SIGTSTP
  
 while true; do 
-	echo -e "\n\n\t\tyouve been haX0red by robotech yoshimaru autonomous nanobots!!\n"
-	echo -e "\t\t\twe pwn u luser!!! l337!!\n"
-	echo -e "+ accessing intertubes (get pkg): \c"
-	spinner '5' '0.1'
-	spinner '7' '0.5'
-	spinner '13' '0.001'
-	echo "done"
-	echo -e "+ preparing pkg: \c"
-	spinner '5' '0.1'
-	spinner '7' '0.5'
-	spinner '13' '0.001'
-	echo "done"
-	echo -e "+ registering...done"
-	echo -e "+ instaling payload: \c"
-	spinner '5' '0.1'
-	spinner '5' '0.5'
-	spinner '15' '0.001'
-	echo "done"
-	echo -e "+ loading scret key: \c"
-	genFakeKey 25
-	echo -e "\n+ finding depencies: \c"
-	spinner '5' '0.1'
-	spinner '5' '0.5'
-	spinner '15' '0.001'
-	echo "done"
-	echo -e "+ opening irc channel (irc.kirin.jp) for commands: \c"
-	spinner '15' '0.1'
-	spinner '8' '0.1'
-	echo "done"
-	echo -e "+ 0-day backdoor: \c"
-	spinner '3' '0.1'
-	spinner '2' '0.1'
-	echo -e "\n+ listen for traffic: \c"
-	endlessSpinner '0.1'
-	echo
+    echo -e "\n\n\t\tyouve been haX0red by robotech yoshimaru autonomous nanobots!!\n"
+    echo -e "\t\t\twe pwn u luser!!! l337!!\n"
+    echo -e "+ accessing intertubes (get pkg): \c"
+    spinner '5' '0.1'
+    spinner '7' '0.5'
+    spinner '13' '0.001'
+    echo "done"
+    echo -e "+ preparing pkg: \c"
+    spinner '5' '0.1'
+    spinner '7' '0.5'
+    spinner '13' '0.001'
+    echo "done"
+    echo -e "+ registering...done"
+    echo -e "+ instaling payload: \c"
+    spinner '5' '0.1'
+    spinner '5' '0.5'
+    spinner '15' '0.001'
+    echo "done"
+    echo -e "+ loading scret key: \c"
+    genFakeKey 25
+    echo -e "\n+ finding depencies: \c"
+    spinner '5' '0.1'
+    spinner '5' '0.5'
+    spinner '15' '0.001'
+    echo "done"
+    echo -e "+ opening irc channel (irc.kirin.jp) for commands: \c"
+    spinner '15' '0.1'
+    spinner '8' '0.1'
+    echo "done"
+    echo -e "+ 0-day backdoor: \c"
+    spinner '3' '0.1'
+    spinner '2' '0.1'
+    echo -e "\n+ listen for traffic:  \c"
+    endlessSpinner '0.1'
+    echo
 
 done
